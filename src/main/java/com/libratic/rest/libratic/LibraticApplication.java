88b6cb37cfbf3a764e3f6fbbbe614eed3f8f337d
Pedro Miguel Jaen Villalobos <pedro.jaenvilla@gmail.com>
2021-05-11 10:31:10 +0200

    first commit

---
+++ src/main/java/com/libratic/rest/libratic/LibraticApplication.java
@@ -0,0 +1,13 @@
+package com.libratic.rest.libratic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/libratic/rest/libratic/LibraticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libratic.rest.libratic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
